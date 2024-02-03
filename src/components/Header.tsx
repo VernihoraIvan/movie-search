d31e2c85c55b119773e360b0bf04c249a42a4dfe
VernihoraIvan <vernihorai@gmail.com>
2024-02-03 20:11:09 +0100

    header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
-import { NavLink } from "react-router-dom";
+/// <reference types="vite-plugin-svgr/client" />
 
+import { NavLink } from "react-router-dom";
+import Logo from "@/assets/icons/popcorn.svg?react";
 const Header = () => {
   return (
-    <header>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/search">Search</NavLink>
+    <header className="flex items-center py-4 px-4 border-2 bg-darkbg rounded-lg text-white">
+      <NavLink className="mr-60" to="/">
+        <Logo className="h-14 w-14" />
+      </NavLink>
+      <div className="flex gap-10">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/search">Search</NavLink>
+      </div>
     </header>
   );
 };
